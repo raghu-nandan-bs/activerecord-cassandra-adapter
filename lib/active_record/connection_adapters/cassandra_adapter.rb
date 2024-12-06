b20d6db58acaf45fc8d043b81edd918d904089af
raghu nandan <raghu.nandan@freshworks.com>
2024-12-06 21:47:17 +0530

    debug-ar

--- lib/active_record/connection_adapters/cassandra_adapter.rb
+++ lib/active_record/connection_adapters/cassandra_adapter.rb
@@ -80,7 +80,7 @@ module ActiveRecord
         timestamp_columns = []
         table_definition.columns.map do |column|
           puts "checking table definition column: #{column.inspect}"
-          if column.type == "timestamp"
+          if column.type == "timestamp" || column.type == :timestamp
             timestamp_columns << column.name
           end
         end
