119aaa4deb99a30ce655edf02acffbfa5f8bb7f5
raghu nandan <raghu.nandan@freshworks.com>
2024-12-06 22:22:37 +0530

    debug-ar

--- lib/active_record/connection_adapters/cassandra_adapter.rb
+++ lib/active_record/connection_adapters/cassandra_adapter.rb
@@ -79,6 +79,9 @@ module ActiveRecord
         bind_values = binds.map { |bind| bind.value_before_type_cast } || []
 
         values.each_with_index do |value, index|
+
+          evaluated = value.is_a?(String) && value =~ /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{4,}$/
+          puts "value: #{value}, matched? #{evaluated}"
           # fix error: Cassandra::Errors::InvalidError (marshaling error: unable to parse date '2024-12-06 16:25:13.403772': marshaling error: Milliseconds length exceeds expected (6))
           if value.is_a?(String) && value =~ /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{4,}$/
             # Trim milliseconds to 3 digits
