52287884689a81542afcbef5e4b4e874c5176131
raghu nandan <raghu.nandan@freshworks.com>
2024-12-09 12:15:55 +0530

    debug-ar

--- lib/active_cassandra/parser.rb
+++ lib/active_cassandra/parser.rb
@@ -204,8 +204,8 @@ module SqlToCqlParser
       conditions = []
       while current_token && !(current_token.type == :keyword && %w(LIMIT ORDER).include?(current_token.value.upcase)) && !(current_token.type == :symbol && current_token.value == ';')
         left = expect(:identifier).value.split('.').last
-        if operator == 'IS'
-          operator = '='
+        if current_token == 'IS'
+          @tokens[@position] = '='
         end
         operator = expect(:symbol, '=').value # Simplistic: only handling '=' operator
         right = parse_condition_value
