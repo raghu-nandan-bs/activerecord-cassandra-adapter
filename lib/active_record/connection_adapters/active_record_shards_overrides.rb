be4303b71680f35d8aaab25db754c66be9bb14d3
raghu nandan <raghu.nandan@freshworks.com>
2024-12-18 21:51:28 +0530

    debug

--- lib/active_record/connection_adapters/active_record_shards_overrides.rb
+++ lib/active_record/connection_adapters/active_record_shards_overrides.rb
@@ -1,19 +1,11 @@
-module ActiveRecordShards
-  module ConnectionSwitcher
+module CassandraActiveRecordShardOverride
+  module CassandraConnectionSwitcherOverride
     def connection_specification_name
-      puts "[ActiveRecordShards - Override] connection_specification_name"
+      puts "[Cassandra Driver - Override] connection_specification_name"
       super if defined?(super)
-      name
     end
   end
 
-  class ShardSelectionOverrides
-    def resolve_connection_name(sharded: is_sharded?, configurations: configurations)
-      puts "[ActiveRecordShards - Override] resolve_connection_name"
-      super if defined?(super)
-      name
-    end
-  end
-
-  ActiveRecordShards::ShardSelection.prepend(ShardSelectionOverrides)
 end
+
+ActiveRecord::Base.extend(CassandraActiveRecordShardOverride::CassandraConnectionSwitcherOverride)
