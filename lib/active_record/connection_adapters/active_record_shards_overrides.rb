37442442df53b13daa47fa1eb1fd75db32151b1e
raghu nandan <raghu.nandan@freshworks.com>
2024-12-18 15:28:10 +0530

    debug

--- lib/active_record/connection_adapters/active_record_shards_overrides.rb
+++ lib/active_record/connection_adapters/active_record_shards_overrides.rb
@@ -6,4 +6,14 @@ module ActiveRecordShards
       name
     end
   end
+
+  class ShardSelectionOverrides
+    def resolve_connection_name(sharded: is_sharded?, configurations: configurations)
+      puts "[ActiveRecordShards - Override] resolve_connection_name"
+      super if defined?(super)
+      name
+    end
+  end
+
+  ActiveRecordShards::ShardSelection.prepend(ShardSelectionOverrides)
 end
