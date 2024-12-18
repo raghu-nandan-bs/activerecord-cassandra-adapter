b2c31c963a2a3ab9d46a201d95faaaa0ff4ae439
raghu nandan <raghu.nandan@freshworks.com>
2024-12-18 23:04:31 +0530

    debug

--- lib/active_record/connection_adapters/active_record_shards_overrides.rb
+++ lib/active_record/connection_adapters/active_record_shards_overrides.rb
@@ -1,13 +1,29 @@
 require 'active_record_shards/connection_switcher-6-1'
 
+puts "[DEBUG] Starting override definition"
+
 module CassandraActiveRecordShardOverride
   module CassandraConnectionSwitcherOverride
+    def self.included(base)
+      puts "[DEBUG] Module included in #{base}"
+    end
+
+    def self.prepended(base)
+      puts "[DEBUG] Module prepended to #{base}"
+    end
+
     def connection_specification_name
       puts "[Cassandra Driver - Override] connection_specification_name"
       super if defined?(super)
     end
   end
-
 end
 
-ActiveRecordShards::ConnectionSwitcher.prepend(CassandraActiveRecordShardOverride::CassandraConnectionSwitcherOverride)
+if defined?(ActiveRecordShards::ConnectionSwitcher)
+  puts "[DEBUG] Found ActiveRecordShards::ConnectionSwitcher, applying override"
+  ActiveRecordShards::ConnectionSwitcher.prepend(CassandraActiveRecordShardOverride::CassandraConnectionSwitcherOverride)
+else
+  puts "[DEBUG] WARNING: ActiveRecordShards::ConnectionSwitcher not found!"
+end
+
+puts "[DEBUG] Override definition completed"
